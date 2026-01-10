51bc0d7129d9f956d3b868ee18d7ed7d8937f692
kumar3000 <sidarthoscar15@gmail.com>
2026-01-10 14:15:02 -0600

    v0.1 Fully Functional Basic NFL Game

---
+++ config.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+teams = pd.read_csv(r'docs/club_data.csv', index_col=0)
--- core/player.py
+++ core/player.py
@@ -1,16 +1,36 @@
-import random
 from .team import Team
+from .typing import type_input, type_effect
+import random
 
 class Player:
     def __init__(self, name: str) -> None:
         self.name = name
         self.rating = 0
+        self.seasons = 0
+
+        # Player stats
+        self.wins = 0
+        self.losses = 0
+        self.ties = 0
+
+        # Post season stats
+        self.super_bowls = []
+        self.post_wins = 0
+        self.post_losses = 0
+        self.post_ties = 0
 
     def Draft(self, team: Team) -> None:
         # Assign team
         self.team = team
-        print('The ' + self.team.name + ' select ' + self.name + '!')
+        print(type_effect('The ' + self.team.name + ' select ' + self.name + '!'))
 
         # Generate rating between 5* and 1* QB
-        self.rating = 5*random.randint(1, 5)
-        print(f'{self.name} is a {int(self.rating / 5)} Star player!')
+        self.rating = random.randint(1, 5)
+        print(type_effect(f'{self.name} is a {int(self.rating)} Star player!'))
+
+    def DisplayStats(self) -> None:
+        print(type_effect(f'[CAREER RECORD {self.wins}-{self.losses}-{self.ties}]'))
+        print(type_effect(f'[POST SEASON RECORD {self.post_wins}-{self.post_losses}]'))
+        print(type_effect(f'[SUPER BOWLS]'), end=' ')
+        for super_bowl in self.super_bowls:
+            print(type_effect(f'{super_bowl}]'), end=' ')
