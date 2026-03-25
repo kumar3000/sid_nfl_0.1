1fa2c6e016208d9e22c4f5fb75ab63a21fba8b5e
kumar3000 <sidarthoscar15@gmail.com>
2026-03-25 13:48:11 -0400

    +Mini_Game() function to calculate player tds more interactively

--- core/game.py
+++ core/game.py
@@ -1,6 +1,32 @@
 from .team import Team
 from .player import Player
 import random
+import time
+
+# Mini game that returns the # of touchdowns a player has thrown in the game
+def Mini_Game(player: Player) -> int:
+    p_tds = 0 # Worst case scenario
+
+    # Game sim
+    delay = random.randint(1, 5)
+    print("Driving...")
+    time.sleep(delay)
+    t = time.time() # Start time
+    input("PASS!")
+    score = time.time() - t
+    print(f"Score: {score:.2f}\n")
+    
+    # TD calculation
+    if score < 0.2:
+        p_tds = 3
+    elif score < 0.35:
+        p_tds = 2
+    elif score < 0.5:
+        p_tds = 1
+
+    player.tds += p_tds
+    player.season_tds += p_tds
+    return p_tds
 
 class Game:
     def __init__(self, player: Player, cpu_team: Team) -> None:
@@ -27,9 +53,8 @@ class Game:
     
     def Play(self, post_season: bool = False) -> bool:
         # Generate random score between 0 and 100 for each team
-        p_tds = random.randint(0, self.player.rating)
-        self.player.season_tds += p_tds
-        self.player.tds += p_tds
+        # p_tds = random.randint(0, self.player.rating)
+        p_tds = Mini_Game(self.player)
         cpu_tds = random.randint(0, self.player.rating - 1)
         p_score = 7 * random.randint(0, cpu_tds) + 7 * p_tds + 3 * random.randint(0, 3)
         cpu_score = 7 * random.randint(0, 2) + 7 * cpu_tds + 3 * random.randint(0, 3)
