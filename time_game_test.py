3aedbb9de27f08f548a23723debd4ff25d45042e
kumar3000 <sidarthoscar15@gmail.com>
2026-03-25 13:48:37 -0400

    removed time_game_test.py

--- time_game_test.py
+++
@@ -1,9 +0,0 @@
-import random, time
-
-# Wait between 2 and 10 seconds
-print("START!")
-
-# Record start time, wait for user press, then print result
-t = time.time()
-input()
-print(f"Score: {time.time() - t:.2f}")   
